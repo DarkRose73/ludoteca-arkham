2ea701366c259eae98e88830d50507e26a1b5876
Joan Salas <joan.salas.silva@gmail.com>
2022-04-27 16:06:14 -0400

    se crea el proyecto

---
+++ src/main/java/com/ludoteca/arkham/LudotecaArkhamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ludoteca.arkham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudotecaArkhamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudotecaArkhamBackendApplication.class, args);
+	}
+
+}
