95960451f708883fe139e917b07de4ec9fb6975c
Joan Salas <joan.salas.silva@gmail.com>
2022-04-28 13:09:18 -0400

    Se crean los metodos para listar juegos por ID y para modificar un juego

--- src/main/java/com/ludoteca/arkham/controlador/JuegoControlador.java
+++ src/main/java/com/ludoteca/arkham/controlador/JuegoControlador.java
@@ -4,13 +4,17 @@ package com.ludoteca.arkham.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ludoteca.arkham.excepciones.ExcepcionRecursoNoEncontrado;
 import com.ludoteca.arkham.modelo.Juego;
 import com.ludoteca.arkham.repositorio.JuegoRepositorio;
 
@@ -37,4 +41,30 @@ public class JuegoControlador {
 	public Juego guardarJuego(@RequestBody Juego juego) {
 		return repo.save(juego);
 	}
+	
+	//Metodo utilizado para buscar un juego por ID
+	@GetMapping("/juegos/{id}")
+	public ResponseEntity<Juego> obtenerJuegoPorId(@PathVariable Integer id){
+		//En caso de que el juego no exista mandamos la excepcion creada
+		Juego juego = repo.findById(id).orElseThrow(() -> new ExcepcionRecursoNoEncontrado("No existe el Juego con el ID: "+id));
+		return ResponseEntity.ok(juego);
+	}
+	
+	
+	@PutMapping("/juegos/{id}")
+	public ResponseEntity<Juego> actualizarJuego(@PathVariable Integer id, @RequestBody Juego detalleJuego){
+		//Buscar el juego por ID
+		Juego juego = repo.findById(id).orElseThrow(() -> new ExcepcionRecursoNoEncontrado("No existe el Juego con el ID: "+id));
+
+		//En caso de que exista, setear los nuevos valores
+		juego.setNombre(detalleJuego.getNombre());
+		juego.setPrecio(detalleJuego.getPrecio());
+		juego.setEdicion(detalleJuego.getEdicion());
+		
+		//Crear un nuevo objeto de clase juego y guardarlo
+		Juego juegoActualizado = repo.save(juego);
+		return ResponseEntity.ok(juegoActualizado);
+	}
 }
+
+
